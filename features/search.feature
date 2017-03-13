0f07dcc45540cec3b04c335db7d9163575a6815d
khalid <kabdul@spartaglobal.co>
2017-03-13 15:16:28 +0000

    initial Commit

---
+++ features/search.feature
@@ -0,0 +1,7 @@
+Feature: Search Google
+  Must be able to search google
+
+  - Must have a search box to type in
+  - Must return a list of results
+
+  Scenario:
---
+++ features/step_definitions/google_step_search.rb
@@ -0,0 +1,15 @@
+Given(/^I'm on the google home page$/) do
+ @b.goto'http://google.com'
+end
+
+When(/^I enter '(.+)' into the search box$/) do |search_term|
+	@b.text_field(id:'lst-ib').set search_term
+  # binding.pry
+end
+When(/^I press enter$/) do
+ @b.send_keys "\n"
+end
+
+Then(/^the first one should contain the word '(.+)'$/) do |search_term|
+  expect(@b.h3.text).to include search_term
+end 
